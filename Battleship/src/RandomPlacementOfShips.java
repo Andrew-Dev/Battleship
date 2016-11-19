0f7f64ae85200cf106518290b20f5151a6276307
amolmoses <amolmoses@gmail.com>
2016-11-18 18:07:59 -0700

    Working on randomizing the placement of Battleships.

--- Battleship/src/RandomPlacementOfShips.java
+++ Battleship/src/RandomPlacementOfShips.java
@@ -2,16 +2,32 @@
  * Created by amolmoses on 11/18/16.
  */
 import java.util.Random;
+import java.util.Scanner;
 
 public class RandomPlacementOfShips {
 
     public String getPlacement(int num){
         Random r = new Random();
-        int row = r.nextInt(8);
+        String row = "" + ConvertNumToLetter.numToLetter(r.nextInt(8));
+        System.out.printf("Row=%s\n" , row);
+        int starting = r.nextInt(8);
+        int ending = starting+num;
+        if(ending>7){
+            ending = 7;
+        }
+        System.out.printf("Starting=%d \n Ending=%d" , starting , ending);
 
+        String placement = row+starting + " " + row+ending;
 
+        return placement;
+    }
 
-
-        return null;
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        char ch='A';
+        while(ch!='N') {
+            System.out.println(new RandomPlacementOfShips().getPlacement(4));
+            ch = scanner.nextLine().charAt(0);
+        }
     }
 }
