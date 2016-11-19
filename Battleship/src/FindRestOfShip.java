555b2060a973fa47fa2fa7a0a50aec73d4684096
nyoungstudios <nathanielyoung95070@gmail.com>
2016-11-18 20:26:20 -0500

    finding 2rd, 3rd, etc positions

--- Battleship/src/FindRestOfShip.java
+++ Battleship/src/FindRestOfShip.java
@@ -12,6 +12,7 @@ public class FindRestOfShip {
     public FindRestOfShip(int[] position) {
         this.position = newPosition = position;
         count = 0;
+        result = "";
     }
 
     //0 if miss, 1 if hit
@@ -34,6 +35,38 @@ public class FindRestOfShip {
                 newPosition[0] = position[0];
                 newPosition[1] = position[1] - 1;
                 move(b);
+            } else if (b.grid[position[0] + 1][position[1]] == 1) {
+                newPosition[0] = position[0] - count;
+                newPosition[1] = position[1];
+                if (b.grid[position[0] - 1][position[1]] == 0) {
+                    newPosition[0] = position[0] - 1;
+                    newPosition[1] = position[1];
+                    move(b);
+                }
+            } else if (b.grid[position[0] - 1][position[1]] == 1) {
+                newPosition[0] = position[0] + count;
+                newPosition[1] = position[1];
+                if (b.grid[position[0] + 1][position[1]] == 0) {
+                    newPosition[0] = position[0] - 1;
+                    newPosition[1] = position[1];
+                    move(b);
+                }
+            } else if (b.grid[position[0]][position[1] + 1] == 1) {
+                newPosition[0] = position[0];
+                newPosition[1] = position[1] - count;
+                if (b.grid[position[0]][position[1] - 1] == 0) {
+                    newPosition[0] = position[0] - 1;
+                    newPosition[1] = position[1];
+                    move(b);
+                }
+            } else if (b.grid[position[0]][position[1] - 1] == 1) {
+                newPosition[0] = position[0];
+                newPosition[1] = position[1] + count;
+                if (b.grid[position[0]][position[1] + 1] == 0) {
+                    newPosition[0] = position[0] - 1;
+                    newPosition[1] = position[1];
+                    move(b);
+                }
             }
         } catch (ArrayIndexOutOfBoundsException e) {
 
@@ -46,6 +79,7 @@ public class FindRestOfShip {
         result = b.placeMove(attackSpot);
         if (result.equals("Hit") || result.equals("Sunk")) {
             b.grid[newPosition[0]][newPosition[1]] = 1;
+            count++;
         } else {
             b.grid[newPosition[0]][newPosition[1]] = 0;
         }
