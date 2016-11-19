cc2f57a91de5df609c46f344053e1aaad2e02ffe
andrew <andrew@andrewarpasi.com>
2016-11-18 19:56:22 -0500

    Attack decider thingy

--- Battleship/src/DecideAttack.java
+++ Battleship/src/DecideAttack.java
@@ -3,12 +3,23 @@
  */
 public class DecideAttack {
 
-    public void runAttacks(Battleship b) {
-        for(int i = 0; i < 7; i++) {
-            for(int j = 0; j < 7; j++) {
-                
+    public int[] findShip(Battleship b) {
+        for(int i = 0; i < 8; i+=2) {
+            for(int j = 0; j < 8; j+=2) {
+                char letter = ConvertNumToLetter.numToLetter(j);
+                String posStr = "" + letter + i;
+                String result = b.placeMove(posStr);
+                if(result.equals("Hit") || result.equals("Sunk")) {
+                    int[] pos = new int[2];
+                    pos[0] = i;
+                    pos[1] = j;
+                    b.grid[i][j] = 1;
+                } else if(result.equals("Miss")) {
+                    b.grid[i][j] = 0;
+                }
             }
         }
+        return null;
     }
 
 }
