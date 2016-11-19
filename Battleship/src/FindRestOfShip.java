8d41218a427b224b053e9ce59a79250327c7a126
nyoungstudios <nathanielyoung95070@gmail.com>
2016-11-18 20:12:17 -0500

    find next position

--- Battleship/src/FindRestOfShip.java
+++ Battleship/src/FindRestOfShip.java
@@ -6,6 +6,8 @@ public class FindRestOfShip {
     private int[] position;  //position 0 is x, and position 1 is y
     private int[] newPosition;
     private int count;
+    private String attackSpot;
+    private String result;
 
     public FindRestOfShip(int[] position) {
         this.position = newPosition = position;
@@ -16,18 +18,36 @@ public class FindRestOfShip {
     public void nextPosition(Battleship b) {
         try {
 
-            if (b.grid[position[0] + 1][position[0]] == 0) {
-                
-            } else if (b.grid[position[0] - 1][position[0]] == 0) {
-
-            } else if (b.grid[position[0]][position[0] + 1] == 0) {
-
-            } else if (b.grid[position[0]][position[0] - 1] == 0) {
-
+            if (b.grid[position[0] + 1][position[1]] == 0) {
+                newPosition[0] = position[0] + 1;
+                newPosition[1] = position[1];
+                move(b);
+            } else if (b.grid[position[0] - 1][position[1]] == 0) {
+                newPosition[0] = position[0] - 1;
+                newPosition[1] = position[1];
+                move(b);
+            } else if (b.grid[position[0]][position[1] + 1] == 0) {
+                newPosition[0] = position[0];
+                newPosition[1] = position[1] + 1;
+                move(b);
+            } else if (b.grid[position[0]][position[1] - 1] == 0) {
+                newPosition[0] = position[0];
+                newPosition[1] = position[1] - 1;
+                move(b);
             }
         } catch (ArrayIndexOutOfBoundsException e) {
 
         }
-        b.placeMove();
+
+    }
+
+    public void move(Battleship b) {
+        attackSpot = "" + newPosition[0] + newPosition[1];
+        result = b.placeMove(attackSpot);
+        if (result.equals("Hit") || result.equals("Sunk")) {
+            b.grid[newPosition[0]][newPosition[1]] = 1;
+        } else {
+            b.grid[newPosition[0]][newPosition[1]] = 0;
+        }
     }
 }
