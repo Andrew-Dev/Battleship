5c37387580219e99e038cf9b3ef2f2729f0b3e97
andrew <andrew@andrewarpasi.com>
2016-11-18 19:13:30 -0500

    shit, I'm gonna end up on git commits from last night

---
+++ Battleship/src/DecideAttack.java
@@ -0,0 +1,14 @@
+/**
+ * Created by andrew on 11/18/16.
+ */
+public class DecideAttack {
+
+    public void runAttacks(Battleship b) {
+        for(int i = 0; i < 7; i++) {
+            for(int j = 0; j < 7; j++) {
+                
+            }
+        }
+    }
+
+}
